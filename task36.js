2a15e2999a0ec47df7a43033252068ad14de0f9a
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:54:41 +0500

    task36
    T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.

---
+++ task36.js
@@ -0,0 +1,5 @@
+"use strict";
+function make_shirt(size, message) {
+    console.log(`making a ${size} t shirt with message print on it ${message}`);
+}
+make_shirt("medium", "code is my life");
---
+++ task36.ts
@@ -0,0 +1,4 @@
+function make_shirt(size:string, message:string){
+    console.log(`making a ${size} t shirt with message print on it ${message}`)
+}
+make_shirt("medium","code is my life");
