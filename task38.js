f1c1a94ca4f4264b43dfec121f805e7845a53bd8
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:56:32 +0500

    task38
    Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.

---
+++ task38.js
@@ -0,0 +1,7 @@
+"use strict";
+function describe_city(city, country = "pakistan") {
+    console.log(`${city} in country ${country}`);
+}
+describe_city("karachi");
+describe_city("tokyo", "japan");
+describe_city("lahore");
---
+++ task38.ts
@@ -0,0 +1,7 @@
+function describe_city(city: string, country: string = "pakistan"){
+console.log(`${city} in country ${country}`);
+
+}
+describe_city("karachi")
+describe_city("tokyo","japan");
+describe_city("lahore")
