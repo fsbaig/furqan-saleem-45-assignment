738153264b0ab85772e22fb8d3b3959d04a4ebbe
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:39:17 +0500

    task24
    More Conditional Tests: You don’t have to limit the number of tests you create to 10. If you want to try more comparisons, write more tests. Have at least one True and one False result for each of the following:
    • Tests for equality and inequality with strings
    
    • Tests using the lower case function
    
    • Numerical tests involving equality and inequality, greater than and less than, greater than or equal to, and less than or equal to
    
    • Tests using "and" and "or" operators
    
    • Test whether an item is in a array
    
    • Test whether an item is not in a array

---
+++ task24.ts
@@ -0,0 +1,19 @@
+let fruit = "apple";
+console.log(fruit=="apple")//true
+console.log(fruit=="Apple")//false
+
+//numeric test
+let num1= 2
+let num2= 3
+console.log(num1>num2)//false
+console.log(num2>num1)//true
+//and operator
+console.log((num1<num2)&&(num2>num1))
+//or operator
+console.log((num2<num1)||(num2>num1))
+let fruits= ["apple","bnanna","cherry"];
+console.log("is apple in fruit")
+console.log(fruits.includes("apple"))
+console.log("mango is not a fruit");
+console.log(!fruits.includes("mango"));
+
