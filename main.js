8c7edc6866e98a43584f786302b9a054c520213f
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:29:00 +0500

    task19
    Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.

---
+++ main.js
@@ -0,0 +1,3 @@
+"use strict";
+let guestlist = ["ali", "noor", "arish", "rohan", "moiz"];
+console.log(`i am inviting ${guestlist.length} guests to the dinner`);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let guestlist: string [] = ["ali","noor", "arish", "rohan", "moiz"];
+console.log(`i am inviting ${guestlist.length} guests to the dinner`);
