de47268b4ea46cca1313ccfeab6987d543355fe0
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:33:15 +0500

    task21
    They think of something you could store in a TypeScript Object. Write a program that creates Objects containing these items.

---
+++ task21.ts
@@ -0,0 +1,6 @@
+let car : { MODEL: String; SPEED: String; YEAR: Number}={
+    MODEL: "TOYOTTA COROLLA",
+    SPEED: "120 KPH",
+    YEAR:2023
+};
+console.log(`CAR INFO: ${car.MODEL} AS FAST AS ${car.SPEED}  LAUNCH IN ${car.YEAR}`);
