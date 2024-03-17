7f24426ed44aeb508fbe22cb6d049fc863cb190f
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:47:02 +0500

    task30
    Hello Admin: Make a array of five or more usernames, including the name 'admin'. Imagine you are writing code that will print a greeting to each user after they log in to a website. Loop through the array, and print a greeting to each user:

---
+++ task30.ts
@@ -0,0 +1,10 @@
+let username: string []=["admin","user1","user2","user3","user4","user5",];
+username.forEach(username=>    {    
+    if (username==="admin"){
+        console.log("hello! admin you would like to see status report")
+    }else{
+        console.log(`hello ${username} ethankyou for loging in again`);
+
+    }
+
+});
