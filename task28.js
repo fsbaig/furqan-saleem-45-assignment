b868d478d14780fe7d0e4da37fee9042c468a8ba
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:44:57 +0500

    task2
    Stages of Life: Write an if-else chain that determines a person’s stage of life. Set a value for the variable age, and then:
    • If the person is less than 2 years old, print a message that the person is a baby.
    
    • If the person is at least 2 years old but less than 4, print a message that the person is a toddler.
    
    • If the person is at least 4 years old but less than 13, print a message that the person is a kid.
    
    • If the person is at least 13 years old but less than 20, print a message that the person is a teenager.
    
    • If the person is at least 20 years old but less than 65, print a message that the person is an adult.
    
    • If the person is age 65 or older, print a message that the person is an elder

---
+++ task28.js
@@ -0,0 +1,20 @@
+"use strict";
+let age = 24;
+if (age < 3) {
+    console.log("you are baby");
+}
+else if (age < 4) {
+    console.log("you are todler");
+}
+else if (age < 12) {
+    console.log("you are kids");
+}
+else if (age < 20) {
+    console.log("you are teen");
+}
+else if (age < 25) {
+    console.log("you are young");
+}
+else if (age < 65) {
+    console.log("you are elder");
+}
