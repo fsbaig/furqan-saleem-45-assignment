2c71076e56bb729061694f2f64538119b53066fd
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:48:48 +0500

    task 31
    No Users: Add an if test to Exercise 28 to make sure the list of users is not empty.
    • If the list is empty, print the message We need to find some users!
    
    • Remove all of the usernames from your array, and make sure the correct message is printed

---
+++ task31.js
@@ -0,0 +1,4 @@
+var username = [];
+if (username.length === 0) {
+    console.log("we need to find some user!");
+}
---
+++ task31.ts
@@ -0,0 +1,7 @@
+let username : string[]=["user2"]
+if (username.length===1){
+console.log("we need to find some user!")
+}
+else{
+    console.log("greing user");
+}
