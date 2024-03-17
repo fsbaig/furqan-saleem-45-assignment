f79daf102bcb27d16147752003eee0dd50d9d5a4
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 22:45:32 +0500

    task 9
    Favorite Number: Store your favorite number in a variable. Then, using that variable, create a message that reveals your favorite number. Print that message.

---
+++ task9.js
@@ -0,0 +1,3 @@
+var myFavoritenumber = 9;
+var message = "my favorite number is ".concat(myFavoritenumber, "!");
+console.log(message);
---
+++ task9.ts
@@ -0,0 +1,3 @@
+const myFavoritenumber: number = 9;
+const message: string = `my favorite number is ${myFavoritenumber}!`;
+console.log(message)
