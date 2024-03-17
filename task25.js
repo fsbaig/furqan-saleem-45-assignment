c128366e18b3408593ffea22994dd689b403c36d
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:40:40 +0500

    task 25
    Alien Colors #1: Imagine an alien was just shot down in a game. Create a variable called alien_color and assign it a value of 'green', 'yellow', or 'red'.
    • Write an if statement to test whether the alien’s color is green. If it is, print a message that the player just earned 5 points.
    
    • Write one version of this program that passes the if test and another that fails. (The version that fails will have no output.)

---
+++ task25.js
@@ -0,0 +1,9 @@
+"use strict";
+let alien_color = "green";
+if (alien_color == "green") {
+    console.log("You just earned 5 points!");
+}
+alien_color = "blue";
+if (alien_color == "green") {
+    console.log("you earned 2 point"); // No output because the condition is false
+}
