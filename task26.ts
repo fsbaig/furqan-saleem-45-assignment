1422dd3a83c9d0d5b3509ef5785f5e63b6f56134
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:41:51 +0500

    task 26
    Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
    • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
    
    • If the alien’s color isn’t green, print a statement that the player just earned 10 points.Alien Colors #2: Choose a color for an alien as you did in Exercise 25, and write an if-else chain.
    • If the alien’s color is green, print a statement that the player just earned 5 points for shooting the alien.
    
    • If the alien’s color isn’t green, print a statement that the player just earned 10 points.

---
+++ task26.ts
@@ -0,0 +1,17 @@
+let alien_color = "green";
+if (alien_color == 'green'){
+    console.log("you just earned 10 points for shoting the allien");
+
+
+}else{
+    console.log("you just earned 5 points")
+}
+alien_color = "blue";
+if (alien_color == 'green'){
+    console.log("you just earned 10 points for shoting the allien");
+
+
+}else{
+    console.log("you just earned 5 points")
+}
+    
