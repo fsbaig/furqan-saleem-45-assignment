89522ed87813dc0815dc6f05fb06978fe9d20e8c
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:57:36 +0500

    task39
    City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
    
    "Lahore, Pakistan"
    
    Call your function with at least three city-country pairs, and print the value that’s returned.

---
+++ task39.js
@@ -0,0 +1,7 @@
+function city_country(city, country) {
+    return "".concat(city, ",").concat(country);
+}
+console.log(city_country("lahore", "pakistan"));
+console.log(city_country("istanbul", "turkey"));
+console.log(city_country("brisbane", "australia"));
+console.log(city_country("washington", "USA"));
---
+++ task39.ts
@@ -0,0 +1,8 @@
+function city_country(city :string, country :string): string{
+    return `${city},${country}`;
+
+}
+console.log(city_country("lahore", "pakistan"));
+console.log(city_country("istanbul", "turkey"));
+console.log(city_country("brisbane", "australia"));
+console.log(city_country("washington", "USA"));
