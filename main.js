fe0a75743ca1d6c30230be003801c38c20cd011e
furqan salee <160034257+fsbaig@users.noreply.github.com>
2024-03-17 23:58:38 +0500

    task40
    Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
-"use strict";
-let guestlist = ["ali", "noor", "arish", "rohan", "moiz"];
-console.log(`i am inviting ${guestlist.length} guests to the dinner`);
+function make_album(artist, title, tracks) {
+    var album = { artist: artist, title: title };
+    if (tracks) {
+        album['tracks'] = tracks;
+    }
+    return album;
+}
+console.log(make_album("Artist One", "The First Album"));
+console.log(make_album("Artist Two", "The Second Album"));
+console.log(make_album("Artist Three", "The Third Album", 12));
--- main.ts
+++ main.ts
@@ -1,2 +1,11 @@
-let guestlist: string [] = ["ali","noor", "arish", "rohan", "moiz"];
-console.log(`i am inviting ${guestlist.length} guests to the dinner`);
+function make_album(artist: string, title: string, tracks?: number) {
+    let album = { artist, title };
+    if (tracks) {
+        album ['tracks'] = tracks;
+    }
+    return album;
+}
+
+console.log(make_album("Artist One", "The First Album"));
+console.log(make_album("Artist Two", "The Second Album"));
+console.log(make_album("Artist Three", "The Third Album", 12));
